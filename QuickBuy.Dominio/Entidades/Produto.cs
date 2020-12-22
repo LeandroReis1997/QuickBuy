d297127de67c5ba0dadabd034e13a26799ebee8b
Leandro <40226937+LeandroReis1997@users.noreply.github.com>
2020-12-22 02:25:03 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
